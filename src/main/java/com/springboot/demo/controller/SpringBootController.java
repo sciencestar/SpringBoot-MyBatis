858c0349ee95b84c80d80ee09e7204ab667ab952
sciencestar <1633696870@qq.com>
2020-03-30 18:49:57 +0800

    集成redis

--- src/main/java/com/springboot/demo/controller/SpringBootController.java
+++ src/main/java/com/springboot/demo/controller/SpringBootController.java
@@ -1,9 +1,16 @@
 package com.springboot.demo.controller;
 
 import com.springboot.demo.service.TestService;
+import com.springboot.demo.util.RedisUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * @ClassName SpringBootController
@@ -14,11 +21,36 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 public class SpringBootController {
+    public static final Logger log = LoggerFactory.getLogger(SpringBootController.class);
     @Autowired
     TestService testService;
 
-    @GetMapping("/hello")
+    /*@GetMapping("/hello")
     public String hello() {
         return testService.test();
+    }*/
+
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @RequestMapping(value = "/hello/{id}")
+    public String hello(@PathVariable(value = "id") String id) {
+        //查询缓存中是否存在
+        boolean hasKey = redisUtils.exists(id);
+        String str = "";
+        if (hasKey) {
+            //获取缓存
+            Object object = redisUtils.get(id);
+            log.info("从缓存获取的数据" + object);
+            str = object.toString();
+        } else {
+            //从数据库中获取信息
+            log.info("从数据库中获取数据");
+            str = testService.test();
+            //数据插入缓存（set中的参数含义：key值，user对象，缓存存在时间10（long类型），时间单位）
+            redisUtils.set(id, str, 10L, TimeUnit.MINUTES);
+            log.info("数据插入缓存" + str);
+        }
+        return str;
     }
 }
